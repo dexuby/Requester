ab374795f66f7ebce7d31856328ce48ccdcf8d35
Dexuby <PxLz@web.de>
2022-01-04 18:33:12 +0100

    Add project files.

---
+++ Requester/Requester.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Text;
+using System.Text.Json;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace RequesterLib
+{
+    public class Requester
+    {
+        private readonly HttpClient _httpClient;
+        private readonly CookieContainer _cookieContainer = new();
+
+        private Requester()
+        {
+            var httpClientHandler = new HttpClientHandler
+            {
+                CookieContainer = _cookieContainer
+            };
+
+            _httpClient = new HttpClient(httpClientHandler);
+        }
+
+        public void AddDefaultHeader(string name, string value) => _httpClient.DefaultRequestHeaders.Add(name, value);
+
+        public void AddCookie(Cookie cookie) => _cookieContainer.Add(cookie);
+
+        public void AddCookie(string domain, string name, string value, string path = "/") =>
+            AddCookie(new Cookie(name, value, path, domain));
+
+        public void AddCookies(params (string, string, string)[] pairs) =>
+            pairs.ToList().ForEach(pair => AddCookie(pair.Item1, pair.Item2, pair.Item3));
+
+        public void UpdateCookie(string domain, string name, string value)
+        {
+            foreach (Cookie cookie in _cookieContainer.GetCookies(new Uri(domain)))
+                if (cookie.Name.Equals(name))
+                    cookie.Value = value;
+        }
+
+        public bool HasCookie(string domain, string name)
+        {
+            foreach (Cookie cookie in _cookieContainer.GetCookies(new Uri(domain)))
+                if (cookie.Name.Equals(name))
+                    return true;
+
+            return false;
+        }
+
+        public void ExpireCookie(string domain, string name)
+        {
+            foreach (Cookie cookie in _cookieContainer.GetCookies(new Uri(domain)))
+                if (cookie.Name.Equals(name))
+                    cookie.Expired = true;
+        }
+
+        public void ExpireCookies(string domain, params string[] names)
+        {
+            var nameSet = new HashSet<string>(names);
+            foreach (Cookie cookie in _cookieContainer.GetCookies(new Uri(domain)))
+                if (nameSet.Contains(cookie.Name))
+                    cookie.Expired = true;
+        }
+
+        public void ExpireCookies(string domain, Predicate<Cookie> shouldExpire)
+        {
+            foreach (Cookie cookie in _cookieContainer.GetCookies(new Uri(domain)))
+                if (shouldExpire.Invoke(cookie))
+                    cookie.Expired = true;
+        }
+
+        public async Task<HttpResponseMessage> GetAsync(string url) => await _httpClient.GetAsync(url);
+
+        public async Task<string> GetStringAsync(string url) => await _httpClient.GetStringAsync(url);
+
+        public async Task<JsonElement> GetJsonElementAsync(string url) =>
+            (await JsonDocument.ParseAsync(await _httpClient.GetStreamAsync(url))).RootElement;
+
+        public async Task<T> GetFromJsonAsync<T>(string url) => await _httpClient.GetFromJsonAsync<T>(url);
+
+        public async Task<HttpResponseMessage> PostAsync(string url, HttpContent content) =>
+            await _httpClient.PostAsync(url, content);
+
+        public async Task<HttpResponseMessage> PostStringContentAsync(string url, string content, Encoding encoding,
+            string mediaType = "application/x-www-form-urlencoded") =>
+            await _httpClient.PostAsync(url, new StringContent(content, encoding, mediaType));
+
+        public async Task<HttpResponseMessage> PostAsJsonAsync<T>(string url, T obj,
+            JsonSerializerOptions serializerOptions = null, CancellationToken cancellationToken = default) =>
+            await _httpClient.PostAsJsonAsync(url, obj, serializerOptions, cancellationToken);
+
+        public async Task<JsonElement> PostAsJsonAndGetJsonElementAsync<T>(string url, T obj,
+            JsonSerializerOptions serializerOptions = null, CancellationToken cancellationToken = default)
+        {
+            var responseMessage = await _httpClient.PostAsJsonAsync(url, obj, serializerOptions, cancellationToken);
+            return (await JsonDocument.ParseAsync(await responseMessage.Content.ReadAsStreamAsync(cancellationToken),
+                cancellationToken: cancellationToken)).RootElement;
+        }
+
+        public async Task<R> PostAsJsonAndGetFromJsonAsync<T, R>(string url, T obj,
+            JsonSerializerOptions serializerOptions = null, CancellationToken cancellationToken = default)
+        {
+            var responseMessage = await _httpClient.PostAsJsonAsync(url, obj, serializerOptions, cancellationToken);
+            return await JsonSerializer.DeserializeAsync<R>(
+                await responseMessage.Content.ReadAsStreamAsync(cancellationToken),
+                cancellationToken: cancellationToken);
+        }
+
+        public async Task<HttpResponseMessage> SendAsync(HttpRequestMessage requestMessage) =>
+            await _httpClient.SendAsync(requestMessage);
+
+        public CookieContainer GetCookieContainer() => _cookieContainer;
+
+        public class Builder
+        {
+            private readonly Requester _requester;
+
+            public Builder()
+            {
+                _requester = new Requester();
+            }
+
+            public Builder WithDefaultHeader(string name, string value)
+            {
+                _requester.AddDefaultHeader(name, value);
+                return this;
+            }
+
+            public Builder WithCookie(Cookie cookie)
+            {
+                _requester.AddCookie(cookie);
+                return this;
+            }
+
+            public Builder WithCookie(string domain, string name, string value, string path = "/")
+            {
+                _requester.AddCookie(domain, name, value, path);
+                return this;
+            }
+
+            public Builder WithCookies(params (string, string, string)[] pairs)
+            {
+                _requester.AddCookies(pairs);
+                return this;
+            }
+
+            public Requester Build() => _requester;
+        }
+    }
+}
---
+++ RequesterTestApplication/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Net;
+using System.Threading.Tasks;
+using RequesterLib;
+
+namespace RequesterTestApplication
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var requester = new Requester.Builder()
+                .Build();
+
+            var test = await requester.GetJsonElementAsync("https://random-data-api.com/api/users/random_user");
+            Console.WriteLine(test);
+        }
+    }
+}
